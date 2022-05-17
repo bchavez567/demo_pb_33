85afd71d8289863a059e74e13db860b4551614f4
bchavez567 <85799655+bchavez567@users.noreply.github.com>
2022-05-17 18:26:10 -0300

    cambio de puerto 0000

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const PORT = process.env.PORT || 0000;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static('public'));
@@ -9,7 +9,7 @@ app.get('/mensaje', (req, res) => {
     res.send('Hola a todos desde Heroku!')
 });
 
-const server = app.listen(PORT, ()=> {
+const server = app.listen(PORT,'0.0.0.0' ,()=> {
     console.log(`Server is run and port => ` , PORT);
 });
 
